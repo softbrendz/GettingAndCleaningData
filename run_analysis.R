56853678640985e16dce2c82d7385aa38814268a
softbrendz <5174415+softbrendz@users.noreply.github.com>
2018-01-09 03:14:32 -0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,123 @@
+## Data download and unzip 
+setwd("D:/Module3")
+##load the httr library
+library(httr)
+## load the data.table library
+library(data.table)
+
+
+## Read Data
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+
+
+
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+features <- read.table("UCI HAR Dataset/features.txt")  
+
+
+
+## Analysis
+
+# 1. Merges the training and the test sets to create one data set.
+
+dataSet <- rbind(X_train,X_test)
+
+## display the result dataSet
+dataSet
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+# Create a vector of only mean and std, use the vector to subset.
+
+MeanStdOnly <- grep("mean()|std()", features[, 2]) 
+
+dataSet <- dataSet[,MeanStdOnly]
+
+## display the result dataSet
+dataSet
+
+
+
+# 4. Appropriately labels the data set with descriptive activity names.
+
+# Create vector of "Clean" feature names by getting rid of "()" apply to the dataSet to rename labels.
+
+CleanFeatureNames <- sapply(features[, 2], function(x) {gsub("[()]", "",x)})
+
+names(dataSet) <- CleanFeatureNames[MeanStdOnly]
+
+
+
+# combine test and train of subject data and activity data, give descriptive lables
+
+subject <- rbind(subject_train, subject_test)
+
+names(subject) <- 'subject'
+
+activity <- rbind(y_train, y_test)
+
+names(activity) <- 'activity'
+
+
+
+# combine subject, activity, and mean and std only data set to create final data set.
+
+dataSet <- cbind(subject,activity, dataSet)
+
+
+## display the result dataSet
+dataSet
+
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+# group the activity column of dataSet, re-name label of levels with activity_levels, and apply it to dataSet.
+
+act_group <- factor(dataSet$activity)
+
+levels(act_group) <- activity_labels[,2]
+
+dataSet$activity <- act_group
+
+
+
+
+
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+
+
+# check if reshape2 package is installed
+
+if (!"reshape2" %in% installed.packages()) {
+  
+  install.packages("reshape2")
+  
+}
+
+library("reshape2")
+
+
+
+# melt data to tall skinny data and cast means. Finally write the tidy data to the working directory as "tidy_data.txt"
+
+baseData <- melt(dataSet,(id.vars=c("subject","activity")))
+
+secondDataSet <- dcast(baseData, subject + activity ~ variable, mean)
+
+names(secondDataSet)[-c(1:2)] <- paste("[mean of]" , names(secondDataSet)[-c(1:2)] )
+
+write.table(secondDataSet, "tidy_data.txt", sep = ",")
+getwd()
